8252ec5d3a880f8912d95d60942c7750e98f9c16
Diego <dlogon@outlook.com>
2024-03-27 10:31:40 -0600

    defalt and guest layout

---
+++ layouts/default.vue
@@ -0,0 +1,212 @@
+<template>
+  <div>
+    <nav class="bg-gray-800">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex items-center justify-between h-16">
+          <div class="flex items-center">
+            <div class="flex-shrink-0">
+              <!-- <img class="h-8 w-8" src="/logo.svg" alt="Logo" /> -->
+            </div>
+            <div class="hidden md:block">
+              <div class="ml-10 flex items-baseline space-x-4">
+                <a
+                  href="#"
+                  class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >Accounts</a
+                >
+                <a
+                  href="#"
+                  class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >Contacts</a
+                >
+                <a
+                  href="#"
+                  class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >Customers</a
+                >
+              </div>
+            </div>
+          </div>
+          <div class="hidden md:block">
+            <div class="ml-4 flex items-center md:ml-6">
+              <div class="ml-3 relative">
+                <div>
+                  <button
+                    @click="
+                      () => {
+                        isDropdownOpen = !isDropdownOpen;
+                        console.log('Clicked on dropdown');
+                      }
+                    "
+                    class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                    id="user-menu"
+                    aria-haspopup="true"
+                  >
+                    <span class="sr-only">Open user menu</span>
+                    <img
+                      class="h-8 w-8 rounded-full"
+                      src="https://i.pravatar.cc/300"
+                      alt="User Avatar"
+                    />
+                  </button>
+                </div>
+                <div
+                  v-if="isDropdownOpen"
+                  class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="user-menu"
+                >
+                  <a
+                    href="#"
+                    @click="logout"
+                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    role="menuitem"
+                    >Logout</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="-mr-2 flex md:hidden">
+            <button
+              @click="toggleMobileMenu"
+              type="button"
+              class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span class="sr-only">Open main menu</span>
+              <svg
+                class="block h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+              <svg
+                class="hidden h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div v-if="isMobileMenuOpen" class="md:hidden" id="mobile-menu">
+        <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          <a
+            href="#"
+            class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >Accounts</a
+          >
+          <a
+            href="#"
+            class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >Contacts</a
+          >
+          <a
+            href="#"
+            class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >Customers</a
+          >
+        </div>
+        <div class="pt-4 pb-3 border-t border-gray-700">
+          <div class="flex items-center px-5">
+            <div class="flex-shrink-0">
+              <img
+                class="h-10 w-10 rounded-full"
+                src="https://i.pravatar.cc/300"
+                alt="User Avatar"
+              />
+            </div>
+            <div class="ml-3">
+              <div class="text-base font-medium leading-none text-white">
+                John Doe
+              </div>
+              <div class="text-sm font-medium leading-none text-gray-400">
+                john.doe@example.com
+              </div>
+            </div>
+            <button
+              @click="toggleMobileDropdown"
+              class="ml-auto bg-gray-800 flex-shrink-0 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+            >
+              <span class="sr-only">View notifications</span>
+              <svg
+                class="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                ></path>
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 8a7 7 0 00-7 7M3 3h1m2 0h10m2 0h1M3 3v2m0 10v2m0-6h2m0-4h2m4 0h2m0 4h2m0 6v2m0-2h1m2 0h10m-2 0h1"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div v-if="isMobileDropdownOpen" class="mt-3 px-2 space-y-1">
+            <a
+              href="#"
+              class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
+              >Logout</a
+            >
+          </div>
+        </div>
+      </div>
+    </nav>
+    <main>
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import { required, email, sameAs, minLength } from "@vuelidate/validators";
+import { useAuthStore, type UserPayloadInterface } from "@/stores/auth";
+import { useVuelidate } from "@vuelidate/core";
+
+const isDropdownOpen = ref(false);
+let isMobileMenuOpen = false;
+let isMobileDropdownOpen = false;
+const authStore = useAuthStore();
+
+const logout = () => {
+  authStore.logUserOut();
+  return navigateTo("/");
+};
+</script>
+
+<style>
+/* Add your custom styles here */
+</style>
